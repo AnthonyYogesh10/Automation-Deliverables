96965598d05b3e225c17a60d1800bb44269324a5
AnthonyYogesh10 <anthonyyogesh10@gmail.com>
2023-01-03 20:34:11 +0530

    report code added

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -21,16 +21,26 @@ def pytest_html_report_title(report):
 # this is used to modify the test result of html report
 def pytest_html_results_table_header(cells):
     del cells[1]
-    cells.insert(0, html.th("S.NO"))
-    cells.insert(1, html.th("ACTION"))
-    cells.insert(2, html.th("EXPECTED OUTPUT"))
-    cells.insert(3, html.th("ACTUAL OUTPUT"))
-    cells.insert(4, html.th("TEST RESULT"))
+    del cells[2]
+    cells.insert(0, html.th("S.no"))
+    cells.insert(1, html.th("Action"))
+    cells.insert(2, html.th("Expected output"))
+    cells.insert(3, html.th("Actual output"))
     cells.pop()
-
 def pytest_html_results_table_row(report, cells):
-    cells.insert(0, html.td())
-    cells.insert(1, html.td())
+    del cells[1]
+    del cells[2]
+    cells.insert(0, html.td(report.serial_no))
+    cells.insert(1, html.td(report.action))
+    cells.insert(2, html.td(report.expected_output))
+    cells.insert(3, html.td(report.actual_output))
     cells.pop()
-
-
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    report.cls.serial_no = item
+    report.action = (["click the search button and use filter to get data"])
+    report.actual_output = (["as per filter data are collected"])
+    report.expected_output ="all data are collected"
+    report.result = str(item.function.__doc__)
